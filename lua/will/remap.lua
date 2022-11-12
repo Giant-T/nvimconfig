cd230242821ba8a1b5694e06538c61f6321a5ea3
William Boudreault <boudreaultwilliam@gmail.com>
2022-11-12 01:20:41 -0500

    Split navigation

--- lua/will/remap.lua
+++ lua/will/remap.lua
@@ -1,3 +1,14 @@
 local nnoremap = require("will.keymap").nnoremap
 
+-- Goto files
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+
+-- Splits
+nnoremap("<leader>vs", "<cmd>vs<CR>")
+nnoremap("<leader>hs", "<cmd>sp<CR>")
+
+-- Move through splits
+nnoremap("<leader>h", "<C-w>h")
+nnoremap("<leader>j", "<C-w>j")
+nnoremap("<leader>k", "<C-w>k")
+nnoremap("<leader>l", "<C-w>l")
