09c3f2b944cb1225676326236b3d954106cb8a37
William Boudreault <boudreaultwilliam@gmail.com>
2022-11-13 00:05:25 -0500

    Ajout ignore de node_module dans telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,18 @@
-local builtin = require("telescope.builtin")
+-- import telescope plugin safely
+local telescope_setup, telescope = pcall(require, "telescope")
+if not telescope_setup then
+  return
+end
+
+local builtin_setup, builtin = pcall(require, "telescope.builtin")
+if not builtin_setup then
+  return
+end
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+telescope.setup({
+    defaults = {
+        file_ignore_patterns = {"node_modules"}
+    }
+})
