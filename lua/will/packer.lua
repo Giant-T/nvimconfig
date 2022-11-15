611caac550b0ea785efb0ace61495c5082b156fd
William Boudreault <boudreaultwilliam@gmail.com>
2022-11-15 00:42:20 -0500

    Added snippets

--- lua/will/packer.lua
+++ lua/will/packer.lua
@@ -35,6 +35,11 @@ return require('packer').startup(function(use)
     use("hrsh7th/nvim-cmp") -- completion plugin
     use("hrsh7th/cmp-buffer") -- source for text in buffer
 
+    -- Snippets
+    use("L3MON4D3/LuaSnip") -- snippet engine
+    use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+    use("rafamadriz/friendly-snippets") -- useful snippets
+
     -- TreeSitter
     use {
         "nvim-treesitter/nvim-treesitter",
