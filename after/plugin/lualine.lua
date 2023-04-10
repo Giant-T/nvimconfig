3a52cc7c4de29b1585eb6741a7b53428738a4523
William Boudreault <boudreaultwilliam@gmail.com>
2023-04-10 17:23:19 -0400

    Modification de la barre lua

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,15 @@
 require('lualine').setup {
     options = {
-        theme = 'gruvbox-material',
+        theme = 'gruvbox_dark',
         component_separators = '',
         section_separators = '',
-    }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'buffers' },
+        lualine_c = { 'branch', 'diff', 'diagnostics' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
