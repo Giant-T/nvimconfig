53d715d736b00e540f6f1d790d84e565ffc42169
William Boudreault <boudreaultwilliam@gmail.com>
2023-04-10 20:30:33 -0400

    Ajout d'un dashboard

--- lua/will/packer.lua
+++ lua/will/packer.lua
@@ -42,14 +42,83 @@ return require("packer").startup(function(use)
             { "hrsh7th/cmp-buffer" },
 
             -- Snippets
-            { "L3MON4D3/LuaSnip" }, -- snippet engine
-            { "saadparwaiz1/cmp_luasnip" }, -- for autocompletion
+            { "L3MON4D3/LuaSnip" },             -- snippet engine
+            { "saadparwaiz1/cmp_luasnip" },     -- for autocompletion
             { "rafamadriz/friendly-snippets" }, -- useful snippets
         },
     })
 
     -- Commenter
     use("numToStr/Comment.nvim")
+
+    -- Startup
+    use({
+        "glepnir/dashboard-nvim",
+        event = "VimEnter",
+        requires = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            local db = require('dashboard')
+            db.setup({
+                theme = 'doom',
+                config = {
+                    header = {
+                        '',
+                        'M\"\"\"\"\"\"\"`YM MM\"\"\"\"\"\"\"\"`M MMP\"\"\"\"\"YMM M\"\"MMMMM\"\"M M\"\"M M\"\"\"\"\"`\'\"\"\"`YM',
+                        'M  mmmm.  M MM  mmmmmmmM M\' .mmm. `M M  MMMMM  M M  M M  mm.  mm.  M',
+                        'M  MMMMM  M M`      MMMM M  MMMMM  M M  MMMMP  M M  M M  MMM  MMM  M',
+                        'M  MMMMM  M MM  MMMMMMMM M  MMMMM  M M  MMMM\' .M M  M M  MMM  MMM  M',
+                        'M  MMMMM  M MM  MMMMMMMM M. `MMM\' .M M  MMP\' .MM M  M M  MMM  MMM  M',
+                        'M  MMMMM  M MM        .M MMb     dMM M     .dMMM M  M M  MMM  MMM  M',
+                        'MMMMMMMMMMM MMMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM MMMM MMMMMMMMMMMMMM',
+                        '',
+                    }, --your header
+                    center = {
+                        {
+                            icon = ' ',
+                            icon_hl = 'Title',
+                            desc = 'Find File           ',
+                            desc_hl = 'String',
+                            key = "f",
+                            keymap = 'CTRL p',
+                            key_hl = 'Number',
+                            action = 'Telescope find_files'
+                        },
+                        {
+                            icon = ' ',
+                            icon_hl = 'Title',
+                            desc = 'Lookup Marks           ',
+                            desc_hl = 'String',
+                            key = "m",
+                            keymap = 'CTRL e',
+                            key_hl = 'Number',
+                            action = 'lua require("harpoon.ui").toggle_quick_menu()'
+                        },
+                        {
+                            icon = ' ',
+                            icon_hl = 'Title',
+                            desc = 'Recent Files           ',
+                            desc_hl = 'String',
+                            key = "r",
+                            key_hl = 'Number',
+                            action = 'Telescope oldfiles'
+                        },
+                        {
+                            icon = ' ',
+                            icon_hl = 'Title',
+                            desc = 'Update Plugins           ',
+                            desc_hl = 'String',
+                            key = "u",
+                            key_hl = 'Number',
+                            action = 'PackerSync'
+                        },
+                    },
+                    footer = {} --your footer
+                }
+            })
+        end,
+    })
 
     -- Themes
     use("ellisonleao/gruvbox.nvim")
