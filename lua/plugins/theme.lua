a5ea960ae2073f25e53cb27fc36676b205dc687d
William Boudreault <boudreaultwilliam@gmail.com>
2023-12-01 23:07:25 -0500

    fix theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,21 +1,26 @@
 local M = {
     "Shatur/neovim-ayu",
     lazy = false,
-    opts = {
-         overrides = {
-            Normal = { bg = "None" },
-            ColorColumn = { bg = "None" },
-            SignColumn = { bg = "None" },
-            Folded = { bg = "None" },
-            FoldColumn = { bg = "None" },
-            CursorLine = { bg = "None" },
-            CursorColumn = { bg = "None" },
-            WhichKeyFloat = { bg = "None" },
-            VertSplit = { bg = "None" },
-        },
-    },
-    config = function(_, opts)
-        require("ayu").setup(opts)
+    config = function(_, _)
+        local colors = require("ayu.colors")
+        colors.generate()
+
+        require("ayu").setup({
+            overrides = {
+                Normal = { bg = "None" },
+                SignColumn = { bg = "None" },
+                Folded = { bg = "None" },
+                FoldColumn = { bg = "None" },
+                CursorLine = { bg = "None" },
+                CursorColumn = { bg = "None" },
+                WhichKeyFloat = { bg = "None" },
+                VertSplit = { bg = "None" },
+                Character = { fg = colors.string },
+                Boolean = { fg = colors.lsp_parameter },
+                Number = { fg = colors.lsp_parameter },
+                FloatNumber = { fg = colors.lsp_parameter },
+            }
+        })
 
         vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guibg=none]])
 
