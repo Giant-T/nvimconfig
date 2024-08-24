ba9fad066bc58e8bbf9961c07e73cdc16c6ed311
William Boudreault <boudreaultwilliam@gmail.com>
2024-08-24 01:53:05 -0400

    telescope find++

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,17 @@ local M = {
             }
         })
 
-        vim.keymap.set("n", "<C-p>", function() require('telescope.builtin').find_files() end)
+        local builtin = require('telescope.builtin')
+        local utils = require('telescope.utils')
+
+        vim.keymap.set("n", "<C-p>", function()
+            local _, ret, _ = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' })
+            if ret == 0 then
+                builtin.git_files()
+            else
+                builtin.find_files()
+            end
+        end)
     end,
 }
 
