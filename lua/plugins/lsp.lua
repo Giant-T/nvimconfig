1dddb9c5692ad786553dcbc66605bf896f08953a
William Boudreault <boudreaultwilliam@gmail.com>
2025-10-22 23:13:36 -0400

    TS lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,10 +8,6 @@ return {
             },
         }
 
-        vim.lsp.config.ts_ls = {
-
-        }
-
         vim.lsp.enable({ "clangd", "ts_ls", "eslint", "omnisharp", "rust_analyzer", "nixd", "tinymist", "lua_ls" })
     end,
 }
